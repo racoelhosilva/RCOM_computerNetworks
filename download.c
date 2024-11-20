15a31f0f77b75640ae252af94cc7456fa85601d8
Process-ing <rbruno.p.o@gmail.com>
2024-11-20 15:26:15 +0000

    Fix connection

--- download.c
+++ download.c
@@ -6,38 +6,60 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#define ATTR_MAX_LENGTH 2048
+#define FTP_PORT 21
+
 typedef struct {
-    char *username;
-    char *password;
-    char *domain;
-    char *path;
+    char username[ATTR_MAX_LENGTH];
+    char password[ATTR_MAX_LENGTH];
+    char domain[ATTR_MAX_LENGTH];
+    char path[ATTR_MAX_LENGTH];
 } FtpUrl;
 
-int parse_url(char *url, FtpUrl *ftp_url) {
-    char *schema = strtok(url, ":"); // Remove scheme
-    char *username_password = strtok(NULL, "@");
-    char *domain = strtok(NULL, "/");
-    char *path = strtok(NULL, "");
 
-    if (schema == NULL || strncmp(username_password, "//", 2) || domain == NULL || path == NULL) {
+int parse_url(char *url, FtpUrl *ftp_url) {
+    char *scheme = strtok(url, ":"); // Remove scheme
+    char *schemeless_url = strtok(NULL, "");
+
+    printf("%s %s\n", scheme, schemeless_url);
+    if (scheme == NULL || strncmp(schemeless_url, "//", 2) != 0) {
         fprintf(stderr, "Bad URL\n");
         return -1;
     }
-    if (strcmp(schema, "ftp") != 0) {
-        fprintf(stderr, "Invalid schema %s\n", schema);
+
+    char *username_password, *domain;
+    if (strchr(schemeless_url + 2, '@') != NULL) {
+        username_password = strtok(schemeless_url + 2, "@");
+        domain = strtok(NULL, "/");
+    } else {
+        username_password = NULL;
+        domain = strtok(schemeless_url + 2, "/");
+    }
+    char *path = strtok(NULL, "");
+
+    if (domain == NULL || path == NULL) {
+        fprintf(stderr, "Bad URL\n");
         return -1;
     }
 
-    if (username_password == NULL
-        || (ftp_url->username = strtok(username_password + 2, ":")) == NULL
-        || (ftp_url->password = strtok(NULL, "")) == NULL) {
-        
-        ftp_url->username = "anonymous";
-        ftp_url->password = "anonymous";
+    if (strcmp(scheme, "ftp") != 0) {
+        fprintf(stderr, "Invalid schema %s\n", scheme);
+        return -1;
     }
 
-    ftp_url->domain = domain;
-    ftp_url->path = path;
+    char *username, *password;
+    if (username_password == NULL
+        || (username = strtok(username_password + 2, ":")) == NULL
+        || (password = strtok(NULL, "")) == NULL) {
+        
+        username = "anonymous";
+        password = "anonymous";
+    }
+
+    strncpy(ftp_url->username, username, ATTR_MAX_LENGTH);
+    strncpy(ftp_url->password, password, ATTR_MAX_LENGTH);
+    strncpy(ftp_url->domain, domain, ATTR_MAX_LENGTH);
+    strncpy(ftp_url->path, path, ATTR_MAX_LENGTH);
 
     return 0;
 }
@@ -89,10 +111,15 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    printf("Username: %s\n", ftp_url.username);
-    printf("Password: %s\n", ftp_url.password);
-    printf("Domain: %s\n", ftp_url.domain);
-    printf("Path: %s\n", ftp_url.path);
+    struct hostent *host = get_host(ftp_url.domain);
+    if (host == NULL) {
+        return 1;
+    }
+
+    int sockfd = get_socket_fd(host, FTP_PORT);
+    if (sockfd < 0) {
+        return 1;
+    }
 
     return 0;
 }
